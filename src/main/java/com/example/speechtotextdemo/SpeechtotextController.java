a40a498d44c0a905e5f2256a18b4bdff9f241c9c
Piotr Garlej <garlej.p@gmail.com>
2021-07-09 09:37:39 +0200

    'init'

---
+++ src/main/java/com/example/speechtotextdemo/SpeechtotextController.java
@@ -0,0 +1,12 @@
+package com.example.speechtotextdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpeechtotextController {
+    @RequestMapping(value = "/")
+    public String home() {
+        return "home";
+    }
+}
---
+++ src/main/java/com/example/speechtotextdemo/SpeechtotextdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.speechtotextdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeechtotextdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeechtotextdemoApplication.class, args);
+	}
+
+}
